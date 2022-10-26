5d8d8273ad5f6f510a71871dcdcf0d012b89c4e7
coronelo <franmoreno1982@gmail.com>
2022-10-26 13:42:10 +0200

    logo

---
+++ logo/logo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+	esc="" # si esto no funciona probar "^[" que es ctrl+v+ESC
+ 
+    negro="${esc}[30m"
+    rojo="${esc}[31m"
+    verde="${esc}[32m"
+    amarillo="${esc}[33m"
+    azul="${esc}[34m"
+    rosa="${esc}[35m"
+    cyan="${esc}[36m"
+    blanco="${esc}[37m"
+ 
+    f_negro="${esc}[40m"
+    f_rojo="${esc}[41m"
+    f_verde="${esc}[42m"
+    f_amarllo="${esc}[43m"
+    f_azul="${esc}[44m"
+    f_rosa="${esc}[45m"
+    f_cyan="${esc}[46m"
+    f_blanco="${esc}[47m"
+ 
+    negrita="${esc}[1m"
+    q_negrita="${esc}[22m"
+    italica="${esc}[3m"
+    q_italica="${esc}[23m"
+    subrayado="${esc}[4m"
+    q_subrayado="${esc}[24m"
+    inverso="${esc}[7m"
+    q_inverso="${esc}[27m"
+ 
+    reset="${esc}[0m"
+
+echo -e "\t\t\t${amarillo}[!]${reset} ${rojo}${negrita}PROYECTO DE:${reset}"
+sleep 0.5
+echo -e "${azul}${negrita}${italica}			LUCIA ORTEGA RODRIGUEZ	${reset}"
+sleep 0.5
+echo -e "${verde}${negrita}${italica}			JAVIER RODRIGUEZ PARRA	${reset}"
+sleep 0.5
+echo -e "${amarillo}${negrita}${italica}			FRANCISCO MORENO RODRIGO	${reset}"
